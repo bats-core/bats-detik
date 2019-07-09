a1285c66420f42ee1a0774a54a36e79587c35137
Vincent Zurczak <vincent.zurczak@gmail.com>
2019-07-09 09:06:35 +0200

    Update docker-build.sh

--- docker-build.sh
+++ docker-build.sh
@@ -8,7 +8,7 @@ version=0.1
 docker build \
 		--build-arg KUBECTL_VERSION=v1.14.1 \
 		--build-arg HELM_VERSION=v2.13.1 \
-		-t vincent-zurczak/devops-e2e-tests-in-kubernetes:latest \
-		-t vincent-zurczak/devops-e2e-tests-in-kubernetes:$version \
+		-t vincent-zurczak/detik:latest \
+		-t vincent-zurczak/detik:$version \
 		.
 
