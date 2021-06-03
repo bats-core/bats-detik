d3dcb8b1ba62a4ab3fee97832f970ff45a8f4615
Vincent Zurczak <vincent.zurczak@gmail.com>
2021-06-03 17:30:19 +0200

    Use BATS Core on Travis

--- docker-build.sh
+++ docker-build.sh
@@ -8,7 +8,6 @@ version=0.2
 docker build \
 		--build-arg KUBECTL_VERSION=v1.14.1 \
 		--build-arg HELM_VERSION=v2.13.1 \
-		-t vincent-zurczak/detik:latest \
-		-t vincent-zurczak/detik:$version \
+		-t bats/bats-detik:latest \
+		-t bats/bats-detik:$version \
 		.
-
